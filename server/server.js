55534fcf5295f0ac9b37709d78064d2558fec85c
Lars Tufvesson <lars.tufvesson@gmail.com>
2019-08-05 01:09:19 +0200

    Hello continents

--- server/server.js
+++ server/server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.listen(3000, function() {
-    console.log('App started on port 3000');
+    console.log('App started on port 3000!');
 });
