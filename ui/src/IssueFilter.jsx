82e18c91756e6a24d8bc8fd2c047a7680566cd41
Lars Tufvesson <lars.tufvesson@gmail.com>
2019-08-15 11:46:20 +0200

    Issue List and Report links highlighted when used

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 /* eslint "react/prefer-stateless-function": "off" */
 
 export default class IssueFilter extends React.Component {
   render() {
     return (
-      <div>This is a placeholder for the issue filter.</div>
+      <div>
+			  <Link to="/issues">All Issues</Link>
+				{' | '}
+			  <Link to={{ pathname: '/issues', search: '?status=New' }}>New Issues</Link>
+				{' | '}
+			  <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>Assigned Issues</Link>
+			</div>
     );
   }
 }
